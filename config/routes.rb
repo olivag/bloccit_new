145faecb7202d28130243211f3f26d22f48b5079
Gerald Oliva <ryumaster001@yahoo.com>
2016-01-20 13:50:29 -0500

    Assignment 33 completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  resources :advertisements, only:[:index, :show]
+
   resources :posts
 
   get 'about' => 'welcome#about'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,8 @@
 require 'faker'
 
-#Create Posts
-50.times do
-  Post.create!(
-    title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
-    )
-end
-
-posts = Post.all
-
-#Create comments
-100.times do
-  Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
-end
+Advertisement.create!(title: "iPod for sale", copy: "Brand new Ipod for sale at half price", price: 99)
+Advertisement.create!(title: "Macbook Pro for sale", copy: "Brand new Macbook Pro for sale at half price", price: 599)
+Advertisement.create!(title: "iMac for sale", copy: "Brand new iMac for sale at half price", price: 1099)
 
 puts "Seeds finished"
-puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+puts "#{Advertisement.count} advertisements created"
