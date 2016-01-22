dcbab1f6f5441a3b26767519a1c5e2ec3357ab05
Gerald Oliva <ryumaster001@yahoo.com>
2016-01-21 21:57:30 -0500

    Checkpoint 36 completed

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '4.2.5'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 gem 'faker'
+gem 'devise'
 gem 'bootstrap-sass'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,3 +1,4 @@
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -38,4 +39,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
