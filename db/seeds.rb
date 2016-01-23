1b567f5393f4fe267e9fb902eebf9a304dcfe142
Gerald Oliva <ryumaster001@yahoo.com>
2016-01-23 17:01:18 -0500

    Checkpoint 38 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,43 @@
 require 'faker'
 
-#Create Posts
+5.times do
+  user = User.new(
+    name: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: Faker::Lorem.characters(10)
+    )
+  user.skip_confirmation!
+  user.save!
+end
+
+users = User.all
+
 50.times do
-  Post.create!(
+  post = Post.create!(
+    user: users.sample,
     title: Faker::Lorem.sentence,
-    body:  Faker::Lorem.paragraph
+    body: Faker::Lorem.paragraph
     )
 end
 
 posts = Post.all
 
-#Create comments
 100.times do
   Comment.create!(
-    post: posts.sample,
-    body: Faker::Lorem.paragraph
-  )
+    body: Faker::Lorem.paragraph,
+    post: posts.sample
+    )
 end
 
-puts "Seeds finished"
+user = User.first
+user.skip_confirmation!
+user.update_attributes!(
+  email: 'ryumaster001@yahoo.com',
+  password: 'fast1234'
+  )
+
+puts "Seed finished"
+puts "#{User.count} users created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
+
