42f708d2807f25f7957cb5161edc6d8a0468ef86
Gerald Oliva <ryumaster001@yahoo.com>
2016-01-23 17:46:34 -0500

    Assignment 38 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,7 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
-  default_scope { order('created_at DESC') }
+  # default_scope { order('created_at DESC') }
+  scope :ordered_by_title, lambda { order('title ASC') }
+  scope :ordered_by_reverse_created_at, lambda { order('created_at ASC') }
 end
